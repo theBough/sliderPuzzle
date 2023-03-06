d83e4dfbe0a66e8832f2e9269bfdb860c4c26d94
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-03-06 15:04:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 title = document.getElementById("title")
 function makeButtons(){
   title.innerHTML = "Give it a sec, we have to do some work first"
+
+  for(i =0 ; i< 15 ; i++){
+      var btn = document.createElement("button");
+      btn.setAttribute("type","button");
+      btn.setAttribute("id" , i);
+      theGrid.appendChild(btn);
+      var first = document.getElementById(i);
+      first.innerHTML = i;   
+  }
+ 
+  
 }
