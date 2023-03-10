db67340a4b2f9c8d0184b9b4522019b63b2542a0
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-03-10 14:50:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,10 @@ function makeButtons(){
   pickStartSpot()
 }
 var thisClicked = function(){
-  console.log(this.id)
   checkAbove(this.id)
+  checkBelow(this.id)
+  checkLeft(this.id)
+  checkRight(this.id)
   
 }
 function checkAbove(thisSquare){
@@ -45,6 +47,68 @@ function checkAbove(thisSquare){
       //this is the colour of all my buttons
       squareAbove.style.backgroundColor = "#00B4D8"
       squareAbove.innerHTML = temp;
+    }
+  }
+}
+function checkBelow(thisSquare){
+  //thisSquare is going to hold the id of the button the 
+  //user pressed.
+  if(thisSquare < 12){
+    //This means we are in rows 2, 3 or 4.
+    var squareBelow = document.getElementById(Number(thisSquare) + 4);
+    var clickedSquare = document.getElementById(thisSquare);
+    
+    if(squareBelow.innerHTML == 0){
+      //the square above is open
+      var temp = clickedSquare.innerHTML;
+      clickedSquare.innerHTML = "0";
+      //this is the colour of the open spot.
+      clickedSquare.style.backgroundColor = "#03045E"
+      //this is the colour of all my buttons
+      squareBelow.style.backgroundColor = "#00B4D8"
+      squareBelow.innerHTML = temp;
+    }
+  }
+}
+function checkLeft(thisSquare){
+  //thisSquare is going to hold the id of the button the 
+  //user pressed.
+  if(thisSquare % 4 != 0){
+    //This means we are in rows 2, 3 or 4.
+    var squareLeft = document.getElementById(Number(thisSquare) -1);
+    var clickedSquare = document.getElementById(thisSquare);
+    
+    if(squareLeft.innerHTML == 0){
+      //the square above is open
+      var temp = clickedSquare.innerHTML;
+      clickedSquare.innerHTML = "0";
+      //this is the colour of the open spot.
+      clickedSquare.style.backgroundColor = "#03045E"
+      //this is the colour of all my buttons
+      squareLeft.style.backgroundColor = "#00B4D8"
+      squareLeft.innerHTML = temp;
+    }
+  }
+}
+function checkRight(thisSquare){
+  //thisSquare is going to hold the id of the button the 
+  //user pressed.
+  console.log(thisSquare)
+  console.log((thisSquare+1) % 4 != 0)
+  if((thisSquare+1) % 4 != 0){
+    //This means we are in rows 2, 3 or 4.
+    var squareRight = document.getElementById(Number(thisSquare) + 1);
+    var clickedSquare = document.getElementById(thisSquare);
+    
+    if(squareRight.innerHTML == 0){
+      //the square above is open
+      var temp = clickedSquare.innerHTML;
+      clickedSquare.innerHTML = "0";
+      //this is the colour of the open spot.
+      clickedSquare.style.backgroundColor = "#03045E"
+      //this is the colour of all my buttons
+      squareRight.style.backgroundColor = "#00B4D8"
+      squareRight.innerHTML = temp;
     }
   }
 }
